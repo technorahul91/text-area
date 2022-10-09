051ab6b8d9bd43c30c71550a95e7886ddad68c5d
technorahul91 <113518342+technorahul91@users.noreply.github.com>
2022-10-09 20:58:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-var maxNum=500;
-function textCount(textField, showCount){
-    if (textField.value.length >maxNum){
-        textField.value=textField.value.substring(0 , maxNum);
+var maxNum = 500;
+function textCount(textField, showCount) {
+    if (textField.value.length > maxNum) {
+        textField.value = textField.value.substring(0, maxNum);
     }
-    else
-    {
-        showCount.value=maxNum-textField.value.length;
+    else {
+        showCount.value = maxNum - textField.value.length;
     }
     return textCount();
 }
+
+
+/*
+setTimeout(function () {
+    window.location.reload(1);
+}, 1000);
+
+*/
+
+
+
+setInterval(()=>{
+    const time=document.querySelector("#time")
+    let date= new Date();
+    let hours= date.getHours();
+    let mins= date.getMinutes();
+    let sec=date.getSeconds();
+    if (hours<10){
+        hours="0"+ hours;
+    }
+
+    if (mins<10){
+        mins="0"+ mins;
+    }
+
+    if (sec<10){
+        sec="0"+ sec;
+    }
+
+
+
+
+    time.textContent= hours + ":" + mins + ":" +sec;
+}
+
+)
