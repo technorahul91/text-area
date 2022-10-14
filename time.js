973b14e22cdc7a07ed4dc6c984c160744e6b403c
progprodigy <115709601+progprodigy@users.noreply.github.com>
2022-10-14 09:11:32 +0530

    Create time.js

---
+++ time.js
@@ -0,0 +1,25 @@
+
+setInterval(() => {
+    const time = document.querySelector("#time")
+    let date = new Date();
+    let hours = date.getHours();
+    let mins = date.getMinutes();
+    let sec = date.getSeconds();
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if (mins < 10) {
+        mins = "0" + mins;
+    }
+
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+
+    time.textContent = hours + ":" + mins + ":" + sec;
+}
+
+)
+
+export default setInterval;
