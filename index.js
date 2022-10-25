ca01d657fbd71555e9e840294c80461dcb7c6901
rahul <technorahul91@gmail.com>
2022-10-25 17:02:34 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+import setInterval from "./time.js";
+
+var maxNum = 500;
+function textCount(textField, showCount) {
+    if (textField.value.length > maxNum) {
+        textField.value = textField.value.substring(0, maxNum);
+    }
+    else {
+        showCount.value = maxNum - textField.value.length;
+    }
+    return textCount();
+}
+
